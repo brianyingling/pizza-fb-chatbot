504931079acddc79d1e51248a73d92a80b794368
Brian Yingling <brian@jet.com>
2019-06-26 14:24:18 -0400

    pizza details and order menu

--- bot.js
+++ bot.js
@@ -6,16 +6,34 @@ const botBuilder = require('claudia-bot-builder');
 const fbTemplate = botBuilder.fbTemplate;
 
 const api = botBuilder(message => {
-    const messageTemplate = new fbTemplate.Generic();
+    if (message.postback) {
+        const [action, pizzaId] = message.text.split('|');
+        if (action === 'DETAILS') {
+            const pizza = pizzas.find(pizza => pizza.id == pizzaId);
+            return [
+                `${pizza.name} has the following ingredients: ` + pizza.ingredients.join(', '),
+                new fbTemplate.Button('What else can I do for you?')
+                    .addButton('Order', `ORDER|${pizzaId}`)
+                    .addButton('Show all pizzas', 'ALL_PIZZAS')
+                    .get()
+            ]
+        } else if (action === 'ORDER') {
+            const pizza = pizzas.find(pizza => pizza.id == pizzaId);
+            return `Thanks for ordering ${pizza.name}! I will let you know as soon as your pizza is ready.`
+        }
+    }
+
+    const reply = new fbTemplate.Generic();
 
     pizzas.forEach(pizza => {
-        messageTemplate.addBubble(pizza.name)
+        reply.addBubble(pizza.name)
             .addImage(pizza.image)
-            .addButton('Details', pizza.id);
+            .addButton('Details', `DETAILS|${pizza.id}`)
+            .addButton('Order', `ORDER|${pizza.id}`)
     });
     return [
         `Hello, here's our pizza menu: `,
-        messageTemplate.get()
+        reply.get()
     ];
 }, {
     platforms: ['facebook']
