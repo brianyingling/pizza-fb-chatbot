fe1e8c7cf0a7b40fb2b1ed878b095766b47af66c
Brian Yingling <brian@jet.com>
2019-06-26 14:06:54 -0400

    using fb template

--- bot.js
+++ bot.js
@@ -3,11 +3,19 @@
 const pizzas = require('./data/pizzas.json');
 
 const botBuilder = require('claudia-bot-builder');
+const fbTemplate = botBuilder.fbTemplate;
 
-const api = botBuilder(() => {
+const api = botBuilder(message => {
+    const messageTemplate = new fbTemplate.Generic();
+
+    pizzas.forEach(pizza => {
+        messageTemplate.addBubble(pizza.name)
+            .addImage(pizza.image)
+            .addButton('Details', pizza.id);
+    });
     return [
-        `Hello, here's our pizza menu: ` + pizzas.map(pizza => pizza.name).join(', '),
-        'Which one do you want?'
+        `Hello, here's our pizza menu: `,
+        messageTemplate.get()
     ];
 }, {
     platforms: ['facebook']
